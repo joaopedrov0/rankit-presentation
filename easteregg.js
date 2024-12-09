f4d65a1767cb999a798c29086de0d629c6b756f6
João Pedro V <mastergamerjp06@gmail.com>
2024-12-08 23:01:44 -0300

    easteregg

---
+++ easteregg.js
@@ -0,0 +1,11 @@
+function reveal(){
+    secret = document.querySelector('.easteregg')
+    secret.hidden = false
+}
+
+function revealSecret(){
+    secret = document.querySelector('.secret')
+    eye = document.querySelector('.eye')
+    eye.hidden = false
+    secret.hidden = false
+}
