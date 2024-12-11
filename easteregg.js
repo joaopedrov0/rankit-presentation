024ac97290d9442074e1befb0ede0ea1ab27899e
João Pedro V <mastergamerjp06@gmail.com>
2024-12-11 13:12:02 +0000

    animação insana

--- easteregg.js
+++ easteregg.js
@@ -1,11 +1,13 @@
 function reveal(){
+    secret = document.querySelector('.secret')
+    secret.classList.toggle('secret-hidden')
     secret = document.querySelector('.easteregg')
-    secret.hidden = false
+    secret.classList.toggle('easteregg-hidden')
 }
 
 function revealSecret(){
     secret = document.querySelector('.secret')
-    eye = document.querySelector('.eye')
-    eye.hidden = false
-    secret.hidden = false
+    // eye = document.querySelector('.eye')
+    // eye.classList.toggle('.eye-hidden')
+    secret.classList.toggle('secret-hidden')
 }
